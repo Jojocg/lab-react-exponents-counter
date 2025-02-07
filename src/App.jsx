0993ccde50d620f0130b43d7002bcf3c8088a27d
Johana Castellano <johanacg16@gmail.com>
2025-02-07 14:29:32 +0000

    done with bonus

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import "./App.css";
 import { useState } from "react";
 
 import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
+import Exponent from "./components/Exponent";
+/* import ExponentTwo from "./components/ExponentTwo";
 import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
-import ExponentSix from "./components/ExponentSix";
+import ExponentSix from "./components/ExponentSix"; */
 
 function App () {
   const [count, setCount] = useState(0);
 
   const decrement = () => setCount((prevCount) => prevCount - 1);
   const increment = () => setCount((prevCount) => prevCount + 1);
+
+  const exponentNumbers = [2, 3, 4, 5, 6];
 
   return (
     <div className="App">
@@ -23,12 +26,14 @@ function App () {
       <br />
       <h2><em>Exponents</em></h2>
 
-      <div className="container">
-        <ExponentTwo count={count}/>
+      <div className="container"> {/* Maybe the key of each Exponent component could be: exponentNumbers.indexOf(num) */}
+        {exponentNumbers.map(num => /* console.log(count) */ <Exponent key={num} baseNumber={count} exponent={num}/>)} 
+        
+        {/* <ExponentTwo count={count}/>
         <ExponentThree count={count}/>
         <ExponentFour count={count}/>
         <ExponentFive count={count}/>
-        <ExponentSix count={count}/>
+        <ExponentSix count={count}/> */}
       </div>
     </div>
   );
--- src/components/Exponent.jsx
+++ src/components/Exponent.jsx
@@ -1,8 +1,16 @@
-export default function Exponent({num}) {
+
+export default function Exponent({ baseNumber, exponent }) {
+    const handleNumRepeat = (exp) => { //this function will print the factors (base number repeated) of exponentiation
+        let repeatedMultiplication = "";
+        for (let i = 0; i < exp; i++) {
+            repeatedMultiplication += (i < exp - 1) ? `${baseNumber} * ` : `${baseNumber}`;
+        }
+        return repeatedMultiplication;
+    }
   return (
     <div className="exponent-counter-container">
-    <p className="exponent-label">{num}^ ²</p>
-    <p className="exponent-result">{num} * {num} = <span className="total">{num * num} </span></p>
+    <p className="exponent-label">{baseNumber}<sup className="exponent-number">{exponent}</sup></p>
+    <p className="exponent-result">{handleNumRepeat(exponent)} = <span className="total">{Math.pow(baseNumber, exponent)}</span></p>
   </div>
   )
 }
